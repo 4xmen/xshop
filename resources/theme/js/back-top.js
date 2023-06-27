456be03358997c92c05cabc667fadbc4e7d7ca2f
Nisay <Ydehfuli@gmail.com>
2023-06-27 13:04:39 +0330

    Fix preloader delay

--- resources/theme/js/back-top.js
+++ resources/theme/js/back-top.js
@@ -1,11 +1,9 @@
 // back to top btn
-$('.backtop-btn').click(function(){
-    $('html, body').animate({ scrollTop: 0 }, 'slow');
+$('.backtop-btn').click(function () {
+    $('html, body').animate({scrollTop: 0}, 'slow');
     return false;
 });
 
 /* pre loader */
-$(window).on('load', function () {
-    $('#preloader').delay(350).fadeOut('slow');
-    $('body').delay(350).css({ 'overflow': 'visible' });
-})
+$('#preloader').delay(450).fadeOut('slow');
+$('body').delay(450).css({'overflow': 'visible'});
