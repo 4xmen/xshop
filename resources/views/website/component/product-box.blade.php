2983294b8e640fd9bdec3dcb3fca810579bbdf95
Nisay <Ydehfuli@gmail.com>
2023-06-30 17:48:56 +0330

    Add fardad product-box.blade.php

--- resources/views/website/component/product-box.blade.php
+++ resources/views/website/component/product-box.blade.php
@@ -1,63 +1,61 @@
-<div class="box">
-    <div class="fav @if($p->isFav()) liked @endif" data-id="{{$p->slug}}">
-        <i class="far fa-heart"></i>
-        <i class="fa fa-heart"></i>
-    </div>
-    @if($p->hasDiscount())
-        <div class="ribbon">
-            <span class="ribbon1"><span>10%</span></span>
-        </div>
-    @endif
-    <a href="{{route('product',$p->slug)}}">
-        <img src="{{$p->thumbUrl()}}" class="img-fluid" alt="{{$p->name}}" title="{{$p->name}}">
-    </a>
-    <a href="{{route('product',$p->slug)}}">
-        <h4>
+{{-- <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4 col-12 p-4">--}}
+{{--     <div class="box">--}}
+{{--         <div class="fav @if($p->isFav()) liked @endif" data-id="{{$p->slug}}">--}}
+{{--             <i class="far fa-heart"></i>--}}
+{{--             <i class="fa fa-heart"></i>--}}
+{{--         </div>--}}
+<div class="card h-100">
+    <img src="{{$p->thumbUrl()}}" class="card-img card-img-top" alt="{{$p->name}}" title="{{$p->name}}">
+    <div class="card-body">
+        <h6 class="card-title">
+            {{$p->category->name}}
+        </h6>
+        <h4 class="card-brand">
             {{$p->name}}
         </h4>
-        <h5>
-            کد محصول:
-           {{$p->getCode()}}
-        </h5>
-        @if(!$p->hasDiscount())
-            @if($p->stock_quantity == 0)
-                <b class="text-danger d-block text-center p-2">
-                    ناموجود
-                </b>
-            @else
-                <b class="d-block text-center p-2">
+        <p class="card-text">
+            {{$p->excerpt}}
+        </p>
+        <div class="price">
+            @if($p->hasDiscount())
+                <del>
+                    {{$p->price}}
+                    {{config('app.currency_type')}}
+                </del>
+                <h4>
                     {{$p->getPrice()}}
-                </b>
-            @endif
-
-        @else
-            @if($p->stock_quantity != 0)
-
-                <div class="row text-center p-3">
-                    <div class="col-md">
-                        <del>
-                            {{$p->price}}
-                            {{config('app.currency_type')}}
-                        </del>
-                    </div>
-                    <div class="col-md">
-
-                        <b> {{$p->getPrice()}}</b>
-                    </div>
-                </div>
+                </h4>
             @else
-                <b class="text-danger d-block text-center">
-                    ناموجود
-                </b>
+                <h4
+                {{$p->getPrice()}}
+                </h4>
             @endif
-        @endif
-    </a>
-    @if($p->stock_quantity > 0)
-        <div class="pb-3 text-center">
-            <a href="{{route('card.add',$p->slug)}}" class="add-to-card btn btn-primary">
-                <i class="icofont-shopping-cart"></i> &nbsp;
-                افزودن به سبد خرید
-            </a>
         </div>
-    @endif
+        @if($p->hasDiscount())
+        <img class="offer" src="{{asset('images/sale.svg')}}" alt="">
+        @endif
+    </div>
+    <div class="card-footer">
+        <div class="text-body-secondary">
+            @if($p->stock_quantity ==0)
+                ناموجود
+            @else
+                <div class="button">
+                    <a href="product.html">
+                        <div class="button-wrapper">
+                            <a href="{{route('card.add',$p->slug)}}" class="add-to-card text">
+                                <i class="icofont-shopping-cart"></i> &nbsp;
+                                افزودن به سبد خرید
+                            </a>
+                            <span class="icon">
+                                <i class="fa fa-basket-shopping"></i>
+                            </span>
+                        </div>
+                    </a>
+                </div>
+            @endif
+        </div>
+    </div>
 </div>
+
+{{--</div>--}}
