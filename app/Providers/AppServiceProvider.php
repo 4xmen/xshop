fb9d071806a3911935241bc2cfb8be0eaa6dc0e1
Nisay <Ydehfuli@gmail.com>
2023-06-30 20:00:42 +0330

    Add fardad ide helper app.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
+        // ...
     }
 
     /**
