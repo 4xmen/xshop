352508117a7237010a737940042457f552b4ee25
A1Gard <a1gard@4xmen.ir>
2024-06-24 06:04:51 +0330

    fixed restore annd trashed bug for product

--- app/Models/Quantity.php
+++ app/Models/Quantity.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Quantity extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 }
