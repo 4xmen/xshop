e280b8deb830d4facb0eb3479f7ea21db55fbf94
A1Gard <a1gard@4xmen.ir>
2024-06-30 08:41:07 +0330

    optimized editor with site template

--- resources/views/components/panel-footer.blade.php
+++ resources/views/components/panel-footer.blade.php
@@ -1,6 +1,9 @@
 @yield('js-content')
 <script type="text/javascript">
     var xupload = "{{route('admin.ckeditor.upload', ['_token' => csrf_token() ])}}";
+    var website_bg = "{{gfx()['background']}}";
+    var website_text_color = "{{gfx()['text']}}";
+    var website_font = "{{gfx()['font']}}";
     window.routesList = @json(getAdminRoutes());
 </script>
 </body>
