7d3b1500d8508965f5d2e509defe27ebe9cc94bd
A1Gard <a1gard@4xmen.ir>
2024-06-30 08:41:07 +0330

    optimized editor with site template

--- resources/js/panel/editor-handle.js
+++ resources/js/panel/editor-handle.js
@@ -1,16 +1,15 @@
-window.addEventListener('load',function () {
+window.addEventListener('load', function () {
     let dirx = 'ltr';
     let editors = {};
     document.querySelectorAll('.ckeditorx')?.forEach(function (el) {
-        editors[el.getAttribute('name')] =  CKEDITOR.replace(el.getAttribute('name'), {
+        editors[el.getAttribute('name')] = CKEDITOR.replace(el.getAttribute('name'), {
             filebrowserUploadUrl: xupload,
             filebrowserUploadMethod: 'form',
             contentsLangDirection: dirx,
             skin: 'moono-dark',
         });
 
-        // WIP: need font and color like website
-        CKEDITOR.addCss('.cke_editable { background-color: #212529; color: white }');
+        CKEDITOR.addCss('.cke_editable { background-color: ' + website_bg + '; color: ' + website_text_color + ' ; font-family: ' + website_font + ' }');
         editors[el.getAttribute('name')].on('change', function (evt) {
             el.value = evt.editor.getData();
         });
