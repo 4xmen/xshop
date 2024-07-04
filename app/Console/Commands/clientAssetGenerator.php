56e7c3924823ac17aced10d2dc81917908132433
A1Gard <a1gard@4xmen.ir>
2024-07-04 04:16:02 +0330

    added client assets

---
+++ app/Console/Commands/clientAssetGenerator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class clientAssetGenerator extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'client';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
