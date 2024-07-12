8c544652feb800f94095bcd3547f9ffd1d25fa6a
A1Gard <a1gard@4xmen.ir>
2024-07-12 07:03:06 +0330

    added fav product with meta theme part

---
+++ resources/views/segments/category/FavProductWithMeta/FavProductWithMeta.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Resources\Views\Segments;
+
+use App\Models\Category;
+use App\Models\Part;
+use App\Models\Setting;
+
+class FavProductWithMeta
+{
+    public static function onAdd(Part $part = null)
+    {
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_title';
+        $setting->value = "Fav product";
+        $setting->type = 'text';
+        $setting->size = 6;
+        $setting->title =  $part->area->name . ' ' . $part->part;
+        $setting->save();
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part;
+        $setting->value = Category::first()->id;
+        $setting->type = 'CATEGORY';
+        $setting->size = 6;
+        $setting->title =  $part->area->name . ' ' . $part->part;
+        $setting->save();
+
+    }
+    public static function onRemove(Part $part = null)
+    {
+
+
+        Setting::where('key',$part->area->name . '_' . $part->part)->first()?->delete();
+        Setting::where('key',$part->area->name . '_' . $part->part.'_title')->first()?->delete();
+    }
+    public static function onMount(Part $part = null)
+    {
+        return $part;
+    }
+}
