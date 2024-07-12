c0e3ed96a09472e3ae61ec7c5245803644c3d6eb
A1Gard <a1gard@4xmen.ir>
2024-07-12 07:03:49 +0330

    added parallax short theme part

---
+++ resources/views/segments/parallax/ParallaxShort/ParallaxShort.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Resources\Views\Segments;
+
+use App\Models\Part;
+use App\Models\Setting;
+use Illuminate\Support\Facades\File;
+
+class ParallaxShort
+{
+    public static function onAdd(Part $part = null)
+    {
+
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_jpg';
+        $setting->value = null;
+        $setting->type = 'FILE';
+        $setting->size = 6;
+        $setting->title =  $part->area->name . ' ' . $part->part.' Image';
+        $setting->save();
+
+        File::copy(__DIR__.'/assets/bg.jpg',public_path('upload/images/').$part->area->name . '.' . $part->part.'.jpg');
+
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_title';
+        $setting->value = 'Title of the parallax';
+        $setting->type = 'TEXT';
+        $setting->size = 6;
+        $setting->title =  $part->area->name . ' ' . $part->part .' title';
+        $setting->save();
+
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_subtitle';
+        $setting->value = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dolor enim reprehenderit.';
+        $setting->type = 'TEXT';
+        $setting->size = 12;
+        $setting->title =  $part->area->name . ' ' . $part->part .' subtitle';
+        $setting->save();
+    }
+    public static function onRemove(Part $part = null)
+    {
+        Setting::where('key',$part->area->name . '_' . $part->part.'_jpg')->first()?->delete();
+        Setting::where('key',$part->area->name . '_' . $part->part.'_title')->first()?->delete();
+        Setting::where('key',$part->area->name . '_' . $part->part.'_subtitle')->first()?->delete();
+        File::delete(public_path('upload/images/').$part->area->name . '.' . $part->part.'.jpg');
+    }
+    public static function onMount(Part $part = null)
+    {
+        return $part;
+    }
+}
