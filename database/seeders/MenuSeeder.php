4ebcecf6463420374d53d1832318258fc18d07f9
A1Gard <a1gard@4xmen.ir>
2024-07-19 04:55:00 +0330

    added menu to setting added menu seeder

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Group;
+use App\Models\Menu;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,5 +15,34 @@ class MenuSeeder extends Seeder
     public function run(): void
     {
         //
+        Menu::factory(['name' => 'main-menu'])->create();
+
+        Menu::first()->items()->createMany([
+           [
+               'title' => "Home",
+               'meta' => '/',
+               'user_id' => 1,
+               'kind'=>'direct',
+           ],
+           [
+               'title' => "News",
+               'menuable_id' => 1,
+               'menuable_type' => Group::class,
+               'user_id' => 1,
+               'kind'=>'direct',
+           ],
+           [
+               'title' => "Contact",
+               'meta' => '/contact-us',
+               'user_id' => 1,
+               'kind'=>'direct',
+           ],
+           [
+               'title' => "About",
+               'meta' => '/about-us',
+               'user_id' => 1,
+               'kind'=>'direct',
+           ],
+        ]);
     }
 }
