0b5eb7697b4ebd7c27bf3b416235170e9fa23054
A1Gard <a1gard@4xmen.ir>
2024-07-20 01:50:20 +0330

    added wave footer

---
+++ resources/views/segments/footer/WaveFooter/WaveFooter.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Resources\Views\Segments;
+
+use App\Models\Part;
+use App\Models\Setting;
+
+class WaveFooter
+{
+    public static function onAdd(Part $part = null)
+    {
+
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_speed';
+        $setting->value = 2;
+        $setting->size = 4;
+        $setting->type = 'NUMBER';
+        $setting->data = json_encode(['xmin' => 2, 'xmax' => 90]);
+        $setting->title =  $part->area->name . ' ' . $part->part. ' speed wave 1';
+        $setting->save();
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_speed2';
+        $setting->value = 6;
+        $setting->size = 4;
+        $setting->type = 'NUMBER';
+        $setting->data = json_encode(['xmin' => 2, 'xmax' => 90]);
+        $setting->title =  $part->area->name . ' ' . $part->part. ' speed wave 2';
+        $setting->save();
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_speed3';
+        $setting->value = 4;
+        $setting->size = 4;
+        $setting->type = 'NUMBER';
+        $setting->data = json_encode(['xmin' => 2, 'xmax' => 90]);
+        $setting->title =  $part->area->name . ' ' . $part->part. ' speed wave 3';
+        $setting->save();
+    }
+    public static function onRemove(Part $part = null)
+    {
+        Setting::where('key',$part->area->name . '_' . $part->part.'_speed')->first()?->delete();
+        Setting::where('key',$part->area->name . '_' . $part->part.'_speed2')->first()?->delete();
+        Setting::where('key',$part->area->name . '_' . $part->part.'_speed3')->first()?->delete();
+    }
+    public static function onMount(Part $part = null)
+    {
+        return $part;
+    }
+}
