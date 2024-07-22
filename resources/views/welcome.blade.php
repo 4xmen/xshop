57ebac9f6af40a27550268520ad50c1c6179d0db
A1Gard <a1gard@4xmen.ir>
2024-07-23 02:21:02 +0330

    added main to website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,10 @@
     welcome
 @endsection
 @section('content')
+    <main>
     @foreach(getParts($area) as $part)
         @php($p = $part->getBladeWithData())
         @include($p['blade'],['data' => $p['data']])
     @endforeach
+    </main>
 @endsection
