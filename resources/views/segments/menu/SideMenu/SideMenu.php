1482829b32dabfaf73c33f5e007d863a2980a1da
A1Gard <a1gard@4xmen.ir>
2024-07-23 06:27:12 +0330

    added side menu

---
+++ resources/views/segments/menu/SideMenu/SideMenu.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Resources\Views\Segments;
+
+use App\Models\Menu;
+use App\Models\Part;
+use App\Models\Setting;
+
+class SideMenu
+{
+    public static function onAdd(Part $part = null)
+    {
+
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_menu';
+        $setting->value = Menu::first()->id;
+        $setting->type = 'MENU';
+        $setting->size = 12;
+        $setting->title =  $part->area->name . ' ' . $part->part .' menu';
+        $setting->save();
+
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_text';
+        $setting->value = 'lorem ipsum';
+        $setting->type = 'EDITOR';
+        $setting->size = 12;
+        $setting->title =  $part->area->name . ' ' . $part->part .' text';
+        $setting->save();
+
+
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_bg';
+        $setting->value = '#ececec';
+        $setting->type = 'COLOR';
+        $setting->size = 3;
+        $setting->title =  $part->area->name . ' ' . $part->part .' background';
+        $setting->save();
+    }
+    public static function onRemove(Part $part = null)
+    {
+        Setting::where('key',$part->area->name . '_' . $part->part.'_menu')->first()?->delete();
+        Setting::where('key',$part->area->name . '_' . $part->part.'_text')->first()?->delete();
+        Setting::where('key',$part->area->name . '_' . $part->part.'bg')->first()?->delete();
+    }
+    public static function onMount(Part $part = null)
+    {
+        return $part;
+    }
+}
