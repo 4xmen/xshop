ce19cd3d779a00d48e358283f7e38e7f2efcb908
A1Gard <a1gard@4xmen.ir>
2024-07-26 00:54:49 +0330

    added simple part

---
+++ resources/views/segments/header/SimpleHeader/SimpleHeader.blade.php
@@ -0,0 +1,10 @@
+<section class='SimpleHeader'>
+    <div class="{{gfx()['container']}}">
+        <h1>
+            Main title
+        </h1>
+        <h2>
+            Subtitle
+        </h2>
+    </div>
+</section>
