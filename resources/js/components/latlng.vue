f2c039e23cfc5be9dcec028bef06154d39e3cf59
A1Gard <a1gard@4xmen.ir>
2024-07-25 05:41:02 +0330

    added location picker to setting

--- resources/js/components/latlng.vue
+++ resources/js/components/latlng.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="latlng">
         <div ref="mapContainer" :style="'height: 300px;'+mapStyle"></div>
+        <template v-if="this.xname != 'NOTHING'">
+            <input type="hidden" :name="xname" :value="allData">
+        </template>
     </div>
 </template>
 
@@ -21,9 +24,15 @@ export default {
             zip: '',
             lat: null,
             lng: null,
+            firstMove: false,
+            currentZoom: 10,
         }
     },
     props: {
+        xname: {
+            default: 'NOTHING',
+            type: String,
+        },
         darkMode: {
             type: Boolean,
             default: false,
@@ -36,7 +45,7 @@ export default {
             type: Number,
             default: 50.99155000,
         },
-        izoom:{
+        izoom: {
             type: Number,
             default: 10,
         },
@@ -45,6 +54,7 @@ export default {
         this.lat = this.ilat;
         this.lng = this.ilng;
         this.zoom = this.izoom;
+        this.currentZoom = this.izoom;
         this.initMap();
     },
     computed: {
@@ -53,7 +63,10 @@ export default {
                 return 'filter: invert(100%) hue-rotate(120deg) brightness(95%) contrast(90%);';
             }
             return '';
-        }
+        },
+        allData() {
+            return this.lat + ',' + this.lng + ',' + this.currentZoom;
+        },
     },
     methods: {
         initMap() {
@@ -66,6 +79,33 @@ export default {
 
             this.map.on('click', this.onMapClick);
             this.map.attributionControl.setPrefix('xShop');
+
+            if (this.marker) {
+                this.map.removeLayer(this.marker);
+            }
+
+            this.marker = L.marker({lat: this.lat, lng: this.lng}).addTo(this.map);
+            // You can emit the selected location or perform any other desired action here
+            // console.log('Selected location:', e.latlng);
+
+            this.map.on("moveend", () => {
+                if (!this.firstMove) {
+                    this.map.invalidateSize();
+                    this.firstMove = true;
+                }
+            });
+            this.map.on("zoomend", (e, x) => {
+                this.map.invalidateSize();
+                this.firstMove = true;
+                this.currentZoom = e.target._zoom;
+            });
+            try {
+                document.querySelector('#lat').value = this.lat;
+                document.querySelector('#lng').value = this.lng;
+            } catch (e) {
+                console.log(e.message);
+            }
+
 
         },
         onMapClick(e) {
@@ -82,7 +122,7 @@ export default {
             try {
                 document.querySelector('#lat').value = this.lat;
                 document.querySelector('#lng').value = this.lng;
-            } catch(e) {
+            } catch (e) {
                 console.log(e.message);
             }
 
