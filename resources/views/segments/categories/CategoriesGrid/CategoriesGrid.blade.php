584a06286724f6d9e68e3b8405d3a00be248038e
A1Gard <a1gard@4xmen.ir>
2024-07-25 03:39:29 +0330

    added grid category

---
+++ resources/views/segments/categories/CategoriesGrid/CategoriesGrid.blade.php
@@ -0,0 +1,22 @@
+<section class='CategoriesGrid'>
+    <div class="{{gfx()['container']}}">
+        <h1>
+            {{getSetting($data->area->name.'_'.$data->part.'_title')}}
+        </h1>
+        <div class="row cat-grid-list">
+            @foreach( getCategorySubCatsBySetting($data->area->name.'_'.$data->part.'_category',getSetting($data->area->name.'_'.$data->part.'_limit')) as $category )
+                <div class="col-md p-0">
+                    <div class="cat-grid-item">
+                        <a href="{{$category->webUrl()}}">
+                            <img src="{{$category->imgUrl()}}" alt="{{$category->name}}"
+                                 title="{{$category->name}}">
+                            <h3 class="py-3">
+                                {{$category->name}}
+                            </h3>
+                        </a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
