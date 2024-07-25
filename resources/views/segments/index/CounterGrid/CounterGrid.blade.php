fd002df592e8d4f73806eb2af631c9c7168695f8
A1Gard <a1gard@4xmen.ir>
2024-07-25 04:43:07 +0330

    added counter theme part

---
+++ resources/views/segments/index/CounterGrid/CounterGrid.blade.php
@@ -0,0 +1,18 @@
+<section id='CounterGrid'>
+    <div class="{{gfx()['container']}}">
+
+        <div class="row">
+            @for($i = 1; $i <= \Resources\Views\Segments\CounterGrid::$count; $i++)
+                <div class="col-lg-3 col-md-6">
+                    <i class="{{getSetting($data->area->name.'_'.$data->part.'_icon'.$i)}}"></i>
+                    <h3>
+                        {{getSetting($data->area->name.'_'.$data->part.'_title'.$i)}}
+                    </h3>
+                    <div class="grid-counter" data-min="0" data-max="{{getSetting($data->area->name.'_'.$data->part.'_number'.$i)}}">
+                        0
+                    </div>
+                </div>
+            @endfor
+        </div>
+    </div>
+</section>
---
+++ resources/views/segments/index/CounterGrid/CounterGrid.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Resources\Views\Segments;
+
+use App\Models\Part;
+use App\Models\Setting;
+
+class CounterGrid
+{
+
+
+    public static  $count = 4;
+    public static function onAdd(Part $part = null)
+    {
+
+
+        for ($i = 1; $i <= self::$count; $i++) {
+
+            $setting = new Setting();
+            $setting->section = 'theme';
+            $setting->key = $part->area->name . '_' . $part->part.'_title'.$i;
+            $setting->value =  "Counter ".$i;
+            $setting->size = 4;
+            $setting->type = 'TEXT';
+//        $setting->data = json_encode(['xmin' => 2, 'xmax' => 90]);
+            $setting->title =  $part->area->name . ' ' . $part->part. ' title '.$i;
+            $setting->save();
+
+            $setting = new Setting();
+            $setting->section = 'theme';
+            $setting->key = $part->area->name . '_' . $part->part.'_icon'.$i;
+            $setting->value = 'ri-user-line';
+            $setting->size = 4;
+            $setting->type = 'ICON';
+//        $setting->data = json_encode(['xmin' => 2, 'xmax' => 6]);
+            $setting->title =  $part->area->name . ' ' . $part->part. ' icon '.$i;
+            $setting->save();
+
+            $setting = new Setting();
+            $setting->section = 'theme';
+            $setting->key = $part->area->name . '_' . $part->part.'_number'.$i;
+            $setting->value = rand(100,4000);
+            $setting->size = 4;
+            $setting->type = 'NUMBER';
+            $setting->data = json_encode(['xmin' => 0, 'xmax' => 9007199254740992]);
+            $setting->title =  $part->area->name . ' ' . $part->part. ' number '.$i;
+            $setting->save();
+        }
+    }
+    public static function onRemove(Part $part = null)
+    {
+        for ($i = 1; $i <= self::$count; $i++) {
+            Setting::where('key',$part->area->name . '_' . $part->part.'_title'.$i)->first()?->delete();
+            Setting::where('key',$part->area->name . '_' . $part->part.'_icon'.$i)->first()?->delete();
+            Setting::where('key',$part->area->name . '_' . $part->part.'_number'.$i)->first()?->delete();
+
+        }
+    }
+    public static function onMount(Part $part = null)
+    {
+        return $part;
+    }
+}
