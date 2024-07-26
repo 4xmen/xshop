b39b4a43d2edc22feb35d99ad6790d7dc16bed51
A1Gard <a1gard@4xmen.ir>
2024-07-26 16:47:28 +0330

    added ParallaxHeaderPin theme part fixed some ui bugs

---
+++ resources/views/segments/header/ParallaxHeaderPin/ParallaxHeaderPin.blade.php
@@ -0,0 +1,13 @@
+<header class='ParallaxHeaderPin' style="background-image: url('{{$bg??asset('upload/images/'.$data->area->name.'.'.$data->part.'.jpg')}}')">
+    <div class="{{gfx()['container']}}">
+        <h1>
+            {{$title}}
+        </h1>
+        <h2>
+            {{$subtitle}}
+        </h2>
+        <div class="fake-container">
+            &nbsp;
+        </div>
+    </div>
+</header>
