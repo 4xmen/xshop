c947b35bf1ac2800fb3fa0cd9e8240016c6ea7d6
A1Gard <a1gard@4xmen.ir>
2024-07-26 16:24:02 +0330

    added simple part added slider

---
+++ resources/views/segments/header/SimpleHeader/SimpleHeader.blade.php
@@ -0,0 +1,10 @@
+<header class='SimpleHeader'>
+    <div class="{{gfx()['container']}}">
+        <h1>
+            {{$title}}
+        </h1>
+        <h2>
+            {{$subtitle}}
+        </h2>
+    </div>
+</header>
