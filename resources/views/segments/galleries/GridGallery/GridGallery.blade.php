f73fa0921f5b536a37df3480a9983059a6d10388
A1Gard <a1gard@4xmen.ir>
2024-07-29 07:00:09 +0330

    added grid gallery to theme part

---
+++ resources/views/segments/galleries/GridGallery/GridGallery.blade.php
@@ -0,0 +1,22 @@
+<section class='GridGallery'>
+    <div class="{{gfx()['container']}}">
+        <h1>
+            {{ getSetting($data->area->name.'_'.$data->part.'_title')}}
+        </h1>
+        <div class="row">
+            @foreach(\App\Models\Gallery::where('status',1)->orderBy('id')->limit( getSetting($data->area->name.'_'.$data->part.'_limit'))->get() as $gallery)
+                <div class="col-md p-1">
+                    <a class="gallery-grid" href="{{$gallery->webUrl()}}">
+
+                        <img src="{{$gallery->imgUrl()}}" alt="{{$gallery->title}}">
+                        <h4>
+                            <span>
+                                {{$gallery->title}}
+                            </span>
+                        </h4>
+                    </a>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
---
+++ resources/views/segments/galleries/GridGallery/GridGallery.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Resources\Views\Segments;
+
+use App\Models\Part;
+use App\Models\Setting;
+
+class GridGallery
+{
+    public static function onAdd(Part $part = null)
+    {
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_title';
+        $setting->value = __('Galleries');
+        $setting->type = 'TEXT';
+        $setting->size = 6;
+        $setting->title =  $part->area->name . ' ' . $part->part.' title';
+        $setting->save();
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_limit';
+        $setting->value = 3;
+        $setting->size = 6;
+        $setting->type = 'NUMBER';
+        $setting->data = json_encode(['xmin' => 2, 'xmax' => 6]);
+        $setting->title =  $part->area->name . ' ' . $part->part. ' limit';
+        $setting->save();
+    }
+    public static function onRemove(Part $part = null)
+    {
+        Setting::where('key',$part->area->name . '_' . $part->part.'_title')->first()?->delete();
+        Setting::where('key',$part->area->name . '_' . $part->part.'_limit')->first()?->delete();
+    }
+    public static function onMount(Part $part = null)
+    {
+        return $part;
+    }
+}
