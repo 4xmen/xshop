148880a7546ec4a248c1cd2ed069c876d503b565
A1Gard <a1gard@4xmen.ir>
2024-08-05 23:07:23 +0330

    added autoplay video clips

---
+++ resources/views/segments/clips/AutoPlayClips/AutoPlayClips.blade.php
@@ -0,0 +1,17 @@
+<section class='AutoPlayClips'>
+    <div class="{{gfx()['container']}}">
+        <h1>
+            {{getSetting($data->area->name.'_'.$data->part.'_title')}}
+        </h1>
+        <div class="autoplay-clip-list">
+        @foreach(\App\Models\Clip::where('status',1)->get() as $clip)
+            <div class="autoplay-clip-item">
+                <a href="{{$clip->webUrl()}}">
+                    <video src="{{$clip->fileUrl()}}" poster="{{$clip->imgUrl()}}" muted ></video>
+                </a>
+{{--                <img src="{{$clip->imgUrl()}}" alt="">--}}
+            </div>
+        @endforeach
+        </div>
+    </div>
+</section>
---
+++ resources/views/segments/clips/AutoPlayClips/AutoPlayClips.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Resources\Views\Segments;
+
+use App\Models\Part;
+use App\Models\Setting;
+
+class AutoPlayClips
+{
+    public static function onAdd(Part $part = null)
+    {
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_title';
+        $setting->value = "Clips list";
+        $setting->type = 'text';
+        $setting->size = 6;
+        $setting->title =  $part->area->name . ' ' . $part->part;
+        $setting->save();
+    }
+    public static function onRemove(Part $part = null)
+    {
+        Setting::where('key',$part->area->name . '_' . $part->part.'_title')->first()?->delete();
+    }
+    public static function onMount(Part $part = null)
+    {
+        return $part;
+    }
+}
