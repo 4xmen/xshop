bc3b4f4d61d0dd4379697385e62565269b40e35a
A1Gard <a1gard@4xmen.ir>
2024-08-26 19:30:27 +0330

    added dark-mode

--- resources/views/website/inc/website-head.blade.php
+++ resources/views/website/inc/website-head.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{config('app.locale')}}" @if(langIsRTL(config('app.locale'))) dir="rtl" @else dir="ltr" @endif >
+<html lang="{{config('app.locale')}}" @if(langIsRTL(config('app.locale'))) dir="rtl" @else dir="ltr" @endif @if(gfx()['dark'] == 1) data-bs-theme="dark" @endif >
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
