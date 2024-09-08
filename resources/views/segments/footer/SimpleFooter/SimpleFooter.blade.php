f7f4b50554c3da0093ba3048e5bffe779d4fc3d2
A1Gard <a1gard@4xmen.ir>
2024-09-08 07:47:28 +0330

    added simple footer theme part

---
+++ resources/views/segments/footer/SimpleFooter/SimpleFooter.blade.php
@@ -0,0 +1,51 @@
+<section class='SimpleFooter'>
+    <div class="content">
+        <div class="{{gfx()['container']}}">
+            <div class="row">
+                <div class="col-md-4">
+                    <h3>
+                        {{getSetting($data->area->name.'_'.$data->part.'_title1')}}
+                    </h3>
+                    @foreach( getGroupPostsBySetting($data->area->name.'_'.$data->part.'_group1',5) as $post )
+                        <li>
+                            <a href="{{$post->webUrl()}}">
+                                {{Str::limit($post->title,40)}}
+                            </a>
+                        </li>
+                    @endforeach
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        {{getSetting($data->area->name.'_'.$data->part.'_title2')}}
+                    </h3>
+                    <ul>
+                        @foreach( getGroupPostsBySetting($data->area->name.'_'.$data->part.'_group2',5) as $post )
+                            <li>
+                                <a href="{{$post->webUrl()}}">
+                                    {{Str::limit($post->title,40)}}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    {!! getSetting($data->area->name.'_'.$data->part.'_last') !!}
+                </div>
+            </div>
+        </div>
+        <div class="p2 text-center">
+            <ul class="social text-center">
+                @foreach(getSettingsGroup('social_')??[] as $k => $social)
+                    <li class="d-inline-block mx-2">
+                        <a href="{{$social}}">
+                            <i class="ri-{{$k}}-line"></i>
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+        <p class="text-center">
+            {{getSetting('copyright')}}
+        </p>
+    </div>
+</section>
---
+++ resources/views/segments/footer/SimpleFooter/SimpleFooter.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Resources\Views\Segments;
+
+use App\Models\Group;
+use App\Models\Part;
+use App\Models\Setting;
+
+class SimpleFooter
+{
+    public static function onAdd(Part $part = null)
+    {
+
+
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_title1';
+        $setting->value = 'FAQ';
+        $setting->size = 6;
+        $setting->type = 'TEXT';
+//        $setting->data = json_encode(['xmin' => 2, 'xmax' => 90]);
+        $setting->title =  $part->area->name . ' ' . $part->part. ' title 1';
+        $setting->save();
+
+
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_group1';
+        $setting->value = Group::first()->id;
+        $setting->size = 6;
+        $setting->type = 'GROUP';
+//        $setting->data = json_encode(['xmin' => 2, 'xmax' => 90]);
+        $setting->title =  $part->area->name . ' ' . $part->part. ' group1';
+        $setting->save();
+
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_title2';
+        $setting->value = 'FAQ';
+        $setting->size = 6;
+        $setting->type = 'TEXT';
+//        $setting->data = json_encode(['xmin' => 2, 'xmax' => 90]);
+        $setting->title =  $part->area->name . ' ' . $part->part. ' title 2';
+        $setting->save();
+
+
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_group2';
+        $setting->value = Group::first()->id;
+        $setting->size = 6;
+        $setting->type = 'GROUP';
+//        $setting->data = json_encode(['xmin' => 2, 'xmax' => 90]);
+        $setting->title =  $part->area->name . ' ' . $part->part. ' group2';
+        $setting->save();
+
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area->name . '_' . $part->part.'_last';
+        $setting->value = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid consequuntur culpa cupiditate dignissimos dolor doloremque error facilis ipsum iure officia quam qui, tempora! Fuga harum impedit iusto magnam veniam.';
+        $setting->size = 12;
+        $setting->title =  $part->area->name . ' ' . $part->part. ' last content';
+        $setting->type = 'EDITOR';
+        $setting->save();
+
+
+//        $setting = new Setting();
+//        $setting->section = 'theme';
+//        $setting->key = $part->area->name . '_' . $part->part.'_bg';
+//        $setting->value = '#111111';
+//        $setting->type = 'COLOR';
+//        $setting->data = json_encode(['name' => 'simple-footer-bg']);
+//        $setting->size = 3;
+//        $setting->title =  $part->area->name . ' ' . $part->part .' background';
+//        $setting->save();
+    }
+    public static function onRemove(Part $part = null)
+    {
+
+        Setting::where('key',$part->area->name . '_' . $part->part.'_title1')->first()?->delete();
+        Setting::where('key',$part->area->name . '_' . $part->part.'_group1')->first()?->delete();
+        Setting::where('key',$part->area->name . '_' . $part->part.'_title2')->first()?->delete();
+        Setting::where('key',$part->area->name . '_' . $part->part.'_group2')->first()?->delete();
+        Setting::where('key',$part->area->name . '_' . $part->part.'_last')->first()?->delete();
+//        Setting::where('key',$part->area->name . '_' . $part->part.'_bg')->first()?->delete();
+    }
+    public static function onMount(Part $part = null)
+    {
+        return $part;
+    }
+}
