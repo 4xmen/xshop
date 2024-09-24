de3c40a354e1ffd49a2e610f08bfcb7b77e9e78f
SadeghPM <SadeghPM@users.noreply.github.com>
2024-09-24 22:52:30 +0330

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
     })
+    ->withCommands([
+        \App\Console\Commands\clientAssetGenerator::class,
+    ])
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
