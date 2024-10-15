c316001abf49ca73f4a820ffc8e809a1cc1e4976
A1Gard <a1gard@4xmen.ir>
2024-10-15 08:17:39 +0330

    added main categories theme part

---
+++ resources/views/segments/index/MainCategoriesSlider/MainCategoriesSlider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Resources\Views\Segments;
+
+use App\Models\Part;
+use App\Models\Setting;
+
+class MainCategoriesSlider
+{
+    public static function onAdd(Part $part = null)
+    {
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area_name . '_' . $part->part.'_title';
+        $setting->value = 'Main categories';
+        $setting->type = 'TEXT';
+        $setting->size = 4;
+        $setting->title =  $part->area_name . ' ' . $part->part .' main categories title';
+        $setting->save();
+
+    }
+    public static function onRemove(Part $part = null)
+    {
+        Setting::where('key',$part->area_name . '_' . $part->part.'_title')->first()?->delete();
+
+    }
+    public static function onMount(Part $part = null)
+    {
+        return $part;
+    }
+}
