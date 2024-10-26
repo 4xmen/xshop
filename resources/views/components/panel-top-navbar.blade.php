4e9e3c6543b9630c62ce1871080c981720a2e341
A1Gard <a1gard@4xmen.ir>
2024-10-26 14:16:18 +0330

    fixed some user bug

--- resources/views/components/panel-top-navbar.blade.php
+++ resources/views/components/panel-top-navbar.blade.php
@@ -38,7 +38,7 @@
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
+                        <a class="dropdown-item" href="{{ route('admin.logout') }}"
                            onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
