199f28935927ec32d35e41552bb23ac1f997e83e
A1Gard <a1gard@4xmen.ir>
2024-11-18 06:09:37 +0330

    added Hod header theme part

---
+++ resources/views/segments/header/HodHeader/HodHeader.blade.php
@@ -0,0 +1,19 @@
+<section class='HodHeader'>
+    <div class="{{gfx()['container']}}">
+        <div class="row">
+            <div class="col-md">
+                <h1>
+                    {{$title}}
+                </h1>
+                <h2>
+                    {{$subtitle}}
+                </h2>
+            </div>
+            <div class="col-md-3 text-end">
+                <div id="hod-logo">
+                <img src="{{asset('upload/images/logo.png')}}" alt="logo" >
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
