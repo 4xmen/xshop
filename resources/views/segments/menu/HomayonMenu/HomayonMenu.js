54035e8caa88db7060ae5535d20924d945353862
A1Gard <a1gard@4xmen.ir>
2024-12-04 04:10:54 +0330

    added homayon menu theme part

---
+++ resources/views/segments/menu/HomayonMenu/HomayonMenu.js
@@ -0,0 +1,26 @@
+const scrollBreakpoint = window.innerHeight * 0.2;
+let fixmenu = function () {
+    if (window.scrollY > scrollBreakpoint){
+        document.querySelector('.HomayonMenu').classList.add('fix');
+        document.querySelector('.HomayonMenu').querySelector('.homayon-middle').classList.remove('container');
+    }else{
+        document.querySelector('.HomayonMenu').classList.remove('fix')
+        document.querySelector('.HomayonMenu').querySelector('.homayon-middle').classList.add('container');
+    }
+}
+window.addEventListener('scroll',fixmenu);
+
+
+fixmenu();
+const toggleSideMenu = function (e) {
+    e.preventDefault();
+    if (document.querySelector('.homayon-resp-menu').style.display == 'none'){
+        document.querySelector('.homayon-resp-menu').style.display = 'block';
+    }else{
+        document.querySelector('.homayon-resp-menu').style.display = 'none';
+    }
+};
+document.addEventListener('DOMContentLoaded',function () {
+    document.querySelector('#homa-toggle-menu')?.addEventListener('click',toggleSideMenu);
+});
+
