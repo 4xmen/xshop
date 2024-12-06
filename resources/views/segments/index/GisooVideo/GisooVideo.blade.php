0e79938a6501b5356aaeed555f0144251b734846
A1Gard <a1gard@4xmen.ir>
2024-12-06 22:47:42 +0330

    fixed vido loop

--- resources/views/segments/index/GisooVideo/GisooVideo.blade.php
+++ resources/views/segments/index/GisooVideo/GisooVideo.blade.php
@@ -1,3 +1,3 @@
 <section class='GisooVideo live-setting' data-live="{{$data->area_name.'_'.$data->part}}" >
-    <video src="{{asset('upload/media/'.$data->area_name.'.'.$data->part.'.mp4')}}" autoplay muted ></video>
+    <video src="{{asset('upload/media/'.$data->area_name.'.'.$data->part.'.mp4')}}" autoplay muted loop ></video>
 </section>
