379cb1abfe61f3d5dde6de1342a459a7d900a9d9
A1Gard <a1gard@4xmen.ir>
2024-12-12 02:30:50 +0330

    added grid clip theme part fixed autoplay limit problem

---
+++ resources/views/segments/clips/GridClips/GridClips.blade.php
@@ -0,0 +1,18 @@
+<section class='GridClips'>
+    <div class="{{gfx()['container']}}">
+        <div class="grid-video-container">
+            @foreach(\App\Models\Clip::where('status',1)->orderByDesc('id')->limit(4)->get() as $clip)
+                <div class="grid-clip-item">
+                    <i class="ri-file-video-line"></i>
+                    <a href="{{$clip->webUrl()}}">
+                        <img src="{{$clip->imgUrl()}}" alt="{{$clip->title}}">
+                        <h3>
+                            {{$clip->title}}
+                        </h3>
+                    </a>
+                    {{--                <img src="{{$clip->imgUrl()}}" alt="">--}}
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
