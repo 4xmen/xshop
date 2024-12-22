60efc8830c2144dbedb22219f64e128cf4a5f5c8
A1Gard <a1gard@4xmen.ir>
2024-12-22 23:58:39 +0330

    added products slider theme part

---
+++ resources/views/segments/products/ProductsSlider/ProductsSlider.blade.php
@@ -0,0 +1,15 @@
+<section class='ProductsSlider'>
+    <div class="{{gfx()['container']}}">
+        <h1>
+            {{getSetting($part->area_name . '_' . $part->part.'_title')}}
+        </h1>
+
+        <div class="products-slider">
+            @foreach(getProductsQueryBySetting($part->area_name . '_' . $part->part.'_query') as $product)
+                <div class="slider-content">
+                    @include(\App\Models\Area::where('name','product-grid')->first()->defPart(),compact('product'))
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
