5e7820061adbfcbc74b993da10e385299fdbe0bc
A1Gard <a1gard@4xmen.ir>
2025-02-27 03:03:16 +0330

    added autoplay video to groups added submenu expandable to homayon-menu repsonosive

---
+++ resources/views/segments/group/AutoPlayAttachment/AutoPlayAttachment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Resources\Views\Segments;
+
+use App\Models\Part;
+use App\Models\Setting;
+
+class AutoPlayAttachment
+{
+    public static function onAdd(Part $part = null)
+    {
+        $setting = new Setting();
+        $setting->section = 'theme';
+        $setting->key = $part->area_name . '_' . $part->part.'_title';
+        $setting->value = "Clips list";
+        $setting->type = 'text';
+        $setting->size = 6;
+        $setting->title =  $part->area_name . ' ' . $part->part;
+        $setting->save();
+    }
+    public static function onRemove(Part $part = null)
+    {
+
+    }
+    public static function onMount(Part $part = null)
+    {
+        return $part;
+    }
+}
