97bc53c46c673b843d62dc643df29c140a8fb03f
A1Gard <a1gard@4xmen.ir>
2025-03-01 03:13:30 +0330

    improve js quality

--- resources/views/segments/slider/SliderSimple/SliderSimple.js
+++ resources/views/segments/slider/SliderSimple/SliderSimple.js
@@ -1,30 +1,34 @@
 import {tns} from "tiny-slider/src/tiny-slider";
 
-var sliderSimple ;
+var sliderSimple;
 
 document.addEventListener('DOMContentLoaded', () => {
-  try {
-      document.querySelectorAll('.SliderSimple')?.forEach(function (el) {
-          if (el.classList.contains('.tns-slider')){
-              console.log('ignore');
-              return 'ignore';
-          }
-          sliderSimple = tns({
-              container: el,
-              items: 1,
-              autoplay: true,
-              autoplayButton: false,
-              // nextButton: false,
-              controls: false,
-              autoplayHoverPause: true,
-              mouseDrag: true,
-              slideBy: 1,
-              autoplayTimeout: 5000,
-              // speed:10000,
-          });
-      });
-  } catch {
-  }
+    try {
+        if (document.querySelectorAll('.SliderSimple').length != 0) {
+
+            document.querySelectorAll('.SliderSimple')?.forEach(function (el) {
+                if (el.classList.contains('.tns-slider')) {
+                    console.log('ignore');
+                    return 'ignore';
+                }
+                sliderSimple = tns({
+                    container: el,
+                    items: 1,
+                    autoplay: true,
+                    autoplayButton: false,
+                    // nextButton: false,
+                    controls: false,
+                    autoplayHoverPause: true,
+                    mouseDrag: true,
+                    slideBy: 1,
+                    autoplayTimeout: 5000,
+                    // speed:10000,
+                });
+            });
+        }
+
+    } catch {
+    }
 
 
 });
