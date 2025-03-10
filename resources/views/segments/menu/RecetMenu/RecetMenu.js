dbf22107dc450fbd0935e7ecb5228094c2f7fda3
A1Gard <a1gard@4xmen.ir>
2025-03-10 06:06:38 +0330

    improve recet menu ui/ux

--- resources/views/segments/menu/RecetMenu/RecetMenu.js
+++ resources/views/segments/menu/RecetMenu/RecetMenu.js
@@ -2,5 +2,16 @@ document.addEventListener('DOMContentLoaded',function () {
     document.querySelector('#rect-toggle')?.addEventListener('click',function (e) {
         e.preventDefault();
         document.querySelector('#RecetMenu').classList.toggle('show-menu');
+        setTimeout(function () {
+            document.addEventListener('click', handleDocumentClick);
+        },100);
     });
 })
+
+
+function handleDocumentClick(e) {
+    if (!document.querySelector('#RecetMenu').contains(e.target)) {
+        document.querySelector('#RecetMenu').classList.toggle('show-menu');
+        document.removeEventListener('click', handleDocumentClick);
+    }
+}
