30c067c12c957889b66f4f2f1bb2976887f15aa5
A1Gard <a1gard@4xmen.ir>
2025-03-12 00:00:08 +0330

    fixed api bug

--- resources/js/client-custom/customerActions.js
+++ resources/js/client-custom/customerActions.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function () {
     document.querySelectorAll('.fav-btn')?.forEach(function (el) {
         el.addEventListener('click', async function () {
             let resp = await axios.get(favUrl+this.getAttribute('data-slug'));
-            if (resp.data.success){
+            if (resp.data.OK){
                 this.setAttribute('data-is-fav',resp.data.data);
                 window.$toast.success(resp.data.message);
             }else {
@@ -16,7 +16,7 @@ window.addEventListener('load', function () {
     document.querySelectorAll('.compare-btn')?.forEach(function (el) {
         el.addEventListener('click', async function () {
             let resp = await axios.get(compUrl+this.getAttribute('data-slug'));
-            if (resp.data.success){
+            if (resp.data.OK){
                 window.$toast.success(resp.data.message);
             }else {
                 window.$toast.error("Error!");
@@ -28,7 +28,7 @@ window.addEventListener('load', function () {
         el.addEventListener('click', async function (e) {
             e.preventDefault();
             let resp = await axios.get(this.getAttribute('href'));
-            if (resp.data.success){
+            if (resp.data.OK){
                 window.$toast.success(resp.data.message);
                 document.querySelectorAll('.card-count')?.forEach(function (el2) {
                    el2.innerText = resp.data.data.count;
