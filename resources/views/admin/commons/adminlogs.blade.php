b63b95f012e0569893ecc086cbaff2ff70bbc136
A1Gard <a1gard@4xmen.ir>
2025-10-23 03:00:43 +0330

    fixed end and start date filter

--- resources/views/admin/commons/adminlogs.blade.php
+++ resources/views/admin/commons/adminlogs.blade.php
@@ -209,18 +209,22 @@
                 {{__("Start date")}}
             </label>
             <vue-datetime-picker-input
-                xid="start_date"   xname="start_date" xtitle="{{__("Start date")}}"  :timepicker="true" @if(app()->getLocale() != 'fa')  def-tab="1" xshow="datetime"  @else xshow="pdatetime"  @endif
-
+                xid="start_date"   xname="start_date" xtitle="{{__("Start date")}}" :timepicker="true"
+                @if(app()->getLocale() != 'fa')  def-tab="1" xshow="datetime"  @else xshow="pdatetime"  @endif
+                @if(request()->filled('start_date')) :xvalue="{{request()->input('start_date','')}}" @endif
             ></vue-datetime-picker-input>
         </div>
     </div>
     <div class="mt-2 mb-2">
         <div class="form-group">
             <label for="end_date">
-                {{__("Start date")}}
+                {{__("End date")}}
             </label>
             <vue-datetime-picker-input
-                xid="end_date" xname="end_date" xtitle="{{__("End date")}}"  :timepicker="true" @if(app()->getLocale() != 'fa')  def-tab="1" xshow="datetime"  @else xshow="pdatetime"  @endif
+                xid="end_date" xname="end_date" xtitle="{{__("End date")}}"
+                :timepicker="true" @if(app()->getLocale() != 'fa')  def-tab="1"
+                xshow="datetime"  @else xshow="pdatetime"  @endif
+                @if(request()->filled('end_date')) :xvalue="{{request()->input('end_date','')}}" @endif
 
             ></vue-datetime-picker-input>
         </div>
--- resources/views/admin/customers/customer-list.blade.php
+++ resources/views/admin/customers/customer-list.blade.php
@@ -9,6 +9,32 @@
 @endsection
 @section('filter')
     {{--  Other filters --}}
+    <div class=" mt-2">
+        <div class="form-group">
+            <label for="start_date">
+                {{__("Start date")}}
+            </label>
+            <vue-datetime-picker-input
+                xid="start_date"   xname="start_date" xtitle="{{__("Start date")}}" :timepicker="true"
+                @if(app()->getLocale() != 'fa')  def-tab="1" xshow="datetime"  @else xshow="pdatetime"  @endif
+            @if(request()->filled('start_date')) :xvalue="{{request()->input('start_date','')}}" @endif
+            ></vue-datetime-picker-input>
+        </div>
+    </div>
+    <div class="mt-2 mb-2">
+        <div class="form-group">
+            <label for="end_date">
+                {{__("End date")}}
+            </label>
+            <vue-datetime-picker-input
+                xid="end_date" xname="end_date" xtitle="{{__("End date")}}"
+                :timepicker="true" @if(app()->getLocale() != 'fa')  def-tab="1"
+                xshow="datetime"  @else xshow="pdatetime"  @endif
+                @if(request()->filled('end_date')) :xvalue="{{request()->input('end_date','')}}" @endif
+
+            ></vue-datetime-picker-input>
+        </div>
+    </div>
 @endsection
 @section('bulk')
     {{--    <option value="-"> - </option> --}}
