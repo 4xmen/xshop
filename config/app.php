8aac8df0500b44f397931b9e74a8b23c768531c3
A1Gard <a1gard@4xmen.ir>
2025-10-28 04:32:41 +0330

    updated persian translate

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'xShop'),
-    'version' => env('APP_VERSION', '2.8.0'),
+    'version' => env('APP_VERSION', '3.0.0'),
     'demo' => env('APP_DEMO', false),
     /*
     |--------------------------------------------------------------------------
